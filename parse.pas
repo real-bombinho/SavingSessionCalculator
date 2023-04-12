41414c6d7b9edbaa8327bf19d15e193fd57bf8c2
real-bombinho <37409530+real-bombinho@users.noreply.github.com>
2023-04-12 02:05:35 +0100

    Update parse.pas

--- parse.pas
+++ parse.pas
@@ -78,7 +78,7 @@ begin
   result := LLondon.ToLocalTime(UnixToDateTime(unixTime));
 end;
 
-function parseDaten(const value: string; out target: TDateTime; IsUTC: boolean = true): boolean;
+function parseDate(const value: string; out target: TDateTime; IsUTC: boolean = true): boolean;
 var year, month, day: integer;
     hour, minute, seconds: integer;
     suffix: integer;
@@ -104,6 +104,8 @@ begin
   end;
   if suffix <> 0 then
   begin
+    if length(value.SubString(20, 5)) < 5 then
+      raise Exception.Create('Invalid suffix ' + value.SubString(19, 6));
     incHour(hour, suffix * strToInt(value.Substring(20, 2)));
     incMinute(minute, suffix * strToInt(value.Substring(23, 2)));
   end;
@@ -176,12 +178,12 @@ begin
   FConsumption := strtocurr(sl[0]);
   if Pos('"interval_start"', sl[1]) <> 0 then
   begin
-    s := sl[1].Substring(pos(':', sl[1]) + 1, 20 );
+    s := sl[1].Substring(pos(':', sl[1]) + 1);
     parseDate(s, FFrom);
   end;
   if Pos('"interval_end"', sl[2]) <> 0 then
   begin
-    s := sl[2].Substring(pos(':', sl[2]) + 1, 20 );
+    s := sl[2].Substring(pos(':', sl[2]) + 1);
     parseDate(s, FTill);
   end;
   sl.Free;
@@ -221,12 +223,12 @@ begin
   FInclVAT := strToFloat(sl[1]);
   if Pos('"valid_from"', sl[2]) <> 0 then
   begin
-    s := sl[2].Substring(pos(':', sl[2]) + 1, 20 );
+    s := sl[2].Substring(pos(':', sl[2]) + 1);
     parseDate(s, FFrom);
   end;
   if Pos('"valid_to"', sl[3]) <> 0 then
   begin
-    s := sl[3].Substring(pos(':', sl[3]) + 1, 20 );
+    s := sl[3].Substring(pos(':', sl[3]) + 1);
     parseDate(s, FTill);
   end;
   sl.Free;
